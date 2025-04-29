4f2a04343c116209e86c7dddf18716bab3ae8e7e
socialdistance <pleaseuseurbrain@protonmail.com>
2025-04-29 15:04:07 +0300

    docker-compose.prod

--- migrations/init/initdb.sql
+++ migrations/init/initdb.sql
@@ -1,2 +1,7 @@
-CREATE DATABASE premium_caste;
-GRANT ALL PRIVILEGES ON DATABASE premium_caste to postgres;
+-- GRANT ALL PRIVILEGES ON DATABASE premium_caste TO postgres;
+-- CREATE DATABASE premium_caste;
+SELECT 'CREATE DATABASE premium_caste'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'premium_caste')\gexec
+
+GRANT ALL PRIVILEGES ON DATABASE premium_caste TO postgres;
+
