49e2211909cfe24712a025ac20b9893f67c7a68c
socialdistance <pleaseuseurbrain@protonmail.com>
2025-06-25 21:11:57 +0300

    remove validation required

--- internal/transport/http/dto/media.go
+++ internal/transport/http/dto/media.go
@@ -30,7 +30,7 @@ type CreateMediaGroupRequest struct {
 
 type AttachMediaRequest struct {
 	GroupID  string   `json:"group_id" validate:"required,uuid"`
-	MediaIDs []string `json:"media_id" validate:"required,min=1,dive,uuid4"`
+	MediaIDs []string `json:"media_id"`
 }
 
 type ListGroupMediaRequest struct {
